d553658573b087ecf0e553e176de48af55813ad7
Vivek <vivek@fxdigital.uk>
2020-02-26 23:25:21 +0000

    chore(assets): add splash screen gifs

--- App.js
+++ App.js
@@ -1,15 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Image, View } from "react-native";
 import Navbar from "./Components/Navbar";
 import ChatContainer from "./Components/ChatContainer";
+import { SplashScreen } from "expo";
+import { Asset } from "expo-asset";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Navbar />
-      <ChatContainer />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false
+    };
+  }
+
+  componentDidMount() {
+    SplashScreen.preventAutoHide();
+  }
+
+  _cacheResourcesAsync = async () => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+    const images = [require("./assets/iPhone.png")];
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    await Promise.all(cacheImages);
+    this.setState({ isReady: true });
+  };
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("./assets/animation.gif")}
+            onLoad={this._cacheResourcesAsync}
+          />
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Navbar />
+        <ChatContainer />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
